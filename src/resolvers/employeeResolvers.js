f2682b8bc5dc38c4e176e7c6dfd06ead9b156ab3
MBprograms <106460672+MBprograms@users.noreply.github.com>
2024-02-18 20:23:26 -0500

    resolver functions for graphql

---
+++ src/resolvers/employeeResolvers.js
@@ -0,0 +1,7 @@
+// resolvers/employeeResolvers.js
+
+const Employee = require('../models/employee');
+
+
+
+module.exports = employeeResolvers;
---
+++ src/resolvers/userResolvers.js
@@ -0,0 +1,8 @@
+// resolvers/userResolvers.js
+
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+
+
+module.exports = userResolvers;
